3882b37b801ac9bcf4dd1f0698e2798b7b944ae9
Prayag Gediya <prayaggediya60@gmail.com>
2022-03-17 13:13:14 +0530

    Initial Commit

---
+++ Twiddle/TwiddleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwiddleApp.swift
+//  Twiddle
+//
+//  Created by home on 17/03/22.
+//
+
+import SwiftUI
+
+@main
+struct TwiddleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
